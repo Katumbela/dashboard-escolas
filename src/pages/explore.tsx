68a42ba58a20fd9dbd8154f33f20a81d9b432303
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-07 15:03:41 +0100

    fix: src/pages/explore.tsx. Criação da função Explore em src/pages/explore.tsx; Modificação de código em src/pages/explore.tsx; Alteração de texto em src/pages/explore.tsx; Criação de uma constante em src/pages/explore.tsx; Alteração em componente React em src/pages/explore.tsx; Mudança de texto em um elemento HTML em src/pages/explore.tsx; Adição de um novo estilo inline em src/pages/explore.tsx; Alteração em funções de manipulação de eventos em src/pages/explore.tsx; Alteração de conteúdo em array ou objeto em src/pages/explore.tsx

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -2,200 +2,153 @@ import { useState } from "react";
 import explore from "../styles/explore.module.css";
 import Image from "next/image";
 import { PurpleButton2 } from "@/components/buttons";
+import { InstitutionCard } from "@/components/institute-card/institute-card";
+import { bgs } from "@/utils/image-exporter";
+import { Header } from "@/components/header";
+import Button from "@/components/global/button/button";
+import { routes } from "@/infra";
 
 
 export default function Explore() {
   const [selectedOption, setSelectedOption] = useState("Angola");
 
-  const handleOptionChange = (event: any) => {
-    setSelectedOption(event.target.value);
-  };
+  const [checked, setChecked] = useState('iem')
 
   return (
     <>
-      <div className={explore.container}>
-        <div className={explore.top}>
-          <h2>Selecione uma opção</h2>
+      <div className={'container'}>
+        <Header showBackButton={true} />
+
+        <h2 className="text-2xl font-extrabold text-center">Selecione uma opção</h2>
+        <br />
+        <br />
+        <div className="container flex justify-center gap-5 text-white ">
+          <InstitutionCard
+            title="IEM - Instituições do Ensino Medio"
+            description="Instituições do Ensino Médio"
+            available="980"
+            backgroundImage={bgs.school_choose_1.src}
+            checked={checked === 'iem'}
+            onClick={() => setChecked('iem')}
+          />
+          <InstitutionCard
+            title="IES - Instituições do Ensino Superior"
+            description="Instituições do Ensino Superior"
+            available="130"
+            backgroundImage={bgs.school_choose_2.src}
+            checked={checked === 'ies'}
+            onClick={() => setChecked('ies')}
+          />
         </div>
-        <div className={explore.options}>
-          <div className={explore.op1}>
-            <label>
-              <input
-                type="radio"
-                value="Angola"
-                checked={selectedOption === "Angola"}
-                onChange={handleOptionChange}
-                style={{ display: "none" }} // Esconde o input radio
-              />
-              <div className={explore.first}>
-                <div className={explore.country}>
-                  <p>Angola </p>
-                </div>
-                {selectedOption === "Angola" && (
-                  <div className={explore.check}>
-                    <Image
-                      src="/icons/check.svg"
-                      alt="Selecionado"
-                      width={70}
-                      height={70}
-                    />
-                  </div>
-                )}
-              </div>
-              <div className={explore.second}>
-                <div className={explore.title}>
-                  <p>IEM - Instituições de Ensino Médio</p>
-                </div>
-                <div className={explore.available}>
-                  <p>
-                    <span className={explore.countIem}>980</span> Disponíveis
-                  </p>
-                </div>
-              </div>
-            </label>
-          </div>
-          <div className={explore.op2}>
-            <label>
-              <input
-                type="radio"
-                value="Angola e Brasil"
-                checked={selectedOption === "Angola e Brasil"}
-                onChange={handleOptionChange}
-                style={{ display: "none" }} // Esconde o input radio
-              />
-              <div className={explore.first}>
-                <div className={explore.country}>
-                  <p>Angola, Brasil </p>
-                </div>
-                {selectedOption === "Angola e Brasil" && (
-                  <div className={explore.check}>
-                    <Image
-                      src="/icons/check.svg" // Caminho para o seu ícone azul
-                      alt="Selecionado"
-                      width={50}
-                      height={70}
-                    />
-                  </div>
-                )}
-              </div>
-              <div className={explore.second}>
-                <div className={explore.title}>
-                  <p>IES - Instituições de Ensino Superior</p>
-                </div>
-                <div className={explore.available}>
-                  <p>
-                    <span className={explore.countIem}>230</span> Disponíveis
-                  </p>
-                </div>
-              </div>
-            </label>
-          </div>
-        </div>
+        <br />
         <div className={explore.info}>
           <div className={explore.content}>
-            {selectedOption === "Angola" ? (
+            {checked === "iem" ? (
               <>
                 <h4>Explore opções de ensino médio!</h4>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Preparação Acadêmica</p>
+                  <p className="my-auto">Preparação Acadêmica</p>
                 </div>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Apoio e Orientação</p>
+                  <p className="my-auto">Apoio e Orientação</p>
                 </div>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Ambiente de Crescimento</p>
+                  <p className="my-auto">Ambiente de Crescimento</p>
                 </div>
               </>
             ) : (
               <>
                 <h4>Explore opções de ensino superior!</h4>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Especialização</p>
+                  <p className="my-auto">Especialização</p>
                 </div>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Oportunidades de Carreira</p>
+                  <p className="my-auto">Oportunidades de Carreira</p>
                 </div>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Pesquisa e Inovação</p>
+                  <p className="my-auto">Pesquisa e Inovação</p>
                 </div>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Experiência Universitária</p>
+                  <p className="my-auto">Experiência Universitária</p>
                 </div>
-                <div className={explore.check1}>
+                <div className={'flex gap-2 my-3'}>
                   <Image
                     className={explore.logo}
                     src={"/icons/done.svg"}
                     alt="done"
-                    width={30}
-                    height={60}
+                    width={20}
+                    height={40}
                     priority
                   />
-                  <p>Desenvolvimento Profissional</p>
+                  <p className="my-auto">Desenvolvimento Profissional</p>
                 </div>
               </>
             )}
           </div>
           <div className={explore.btn}>
-            <PurpleButton2
-              description="Explorar"
-              redirect="/search"
-            ></PurpleButton2>
+            <Button
+              className="py-3 w-full"
+              link={routes.SEARCH_SCHOOL_ROUTE}            >
+              Explorar
+            </Button>
+            <br />
+            <br />
           </div>
         </div>
       </div>
