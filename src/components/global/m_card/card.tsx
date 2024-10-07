4f6d3bf2312e1bbe838f9779c68a3d5611fd9913
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-07 11:36:28 +0100

    feat: src/components/global/m_card/card.tsx. Criação de novo arquivo

---
+++ src/components/global/m_card/card.tsx
@@ -0,0 +1,28 @@
+import { NumberUtils } from "@/utils"
+import { extras } from "@/utils/image-exporter"
+
+export interface IMCard {
+  card_number: string
+  balance: number
+  hodler: string
+  provider: string
+}
+
+export function MCard({ balance, card_number, hodler, provider }: IMCard) {
+  return (
+    <div className="w-[359px] font-mono  relative h-[226px] my-8 rounded-3xl mx-auto bg-primary">
+      <img src={extras.m_card} className="-z10" alt="" />
+      <div className="container absolute px-6 pt-[10vh] top-0 bottom-0 left-0 right-0">
+        <h2 className="text-lg text-white">{card_number}</h2>
+        <div className="mt-4 text-center">
+          <b className="text-2xl text-white">{NumberUtils.formatCurrency(balance)}</b>
+        </div>
+        <br />
+        <div className="flex justify-between text-sm text-white">
+          <span className="font-bold">{hodler}</span>
+          <span className="font-bold">{provider}</span>
+        </div>
+      </div>
+    </div>
+  )
+}
