67d6440772d81a5bc67888f7a61360455de45764
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-07 11:36:48 +0100

    feat: src/infra/config/routes.vars.ts. Criação de novo arquivo

---
+++ src/infra/config/routes.vars.ts
@@ -0,0 +1,35 @@
+const HOME_ROUTE = "/",
+    LOGIN_ROUTE = "/login",
+    CARDS_ROUTE = "/cards",
+    APPS_ROUTE = "/apps",
+    DASH_ROUTE = "/dash",
+    PROFILE_ROUTE = "/profile",
+    TUITION_ROUTE = "/tuition",
+    MOVEMENTS_ROUTE = "/movements",
+    REGISTER_ROUTE = "/register",
+    PAYMENTS_ROUTE = "/payments",
+    STEPONE_ROUTE = "/step-one",
+    CHOOSE_SCHOOL_OPTION_ROUTE = "/choose-option",
+    SEARCH_SCHOOL_ROUTE = "/search-school",
+    VIEW_SCHOOL_ROUTE = "/view-school",
+    VIEW_COURSE_ROUTE = "/view-course"
+
+
+export const routes = {
+    HOME_ROUTE,
+    TUITION_ROUTE,
+    PAYMENTS_ROUTE,
+    VIEW_SCHOOL_ROUTE,
+    CHOOSE_SCHOOL_OPTION_ROUTE,
+    SEARCH_SCHOOL_ROUTE,
+    VIEW_COURSE_ROUTE,
+    STEPONE_ROUTE,
+    REGISTER_ROUTE,
+    MOVEMENTS_ROUTE,
+    LOGIN_ROUTE,
+    DASH_ROUTE,
+    PROFILE_ROUTE,
+    CARDS_ROUTE,
+    APPS_ROUTE
+
+} as const 
