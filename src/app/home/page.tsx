79438ef10dc1f22a01128192840cf01960fe09c8
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-12 23:37:59 +0100

    fix: src/app/home/page.tsx. Criação da função HomeLoggedIn em src/app/home/page.tsx; Modificação de código em src/app/home/page.tsx; Criação de uma constante em src/app/home/page.tsx; Alteração em componente React em src/app/home/page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -14,6 +14,7 @@ import { MCard } from "@/components/global/m_card/card";
 import { TransactionList } from "@/components/global/transactionTable/transactrionTable";
 import type { Transaction } from "@/infra/interfacess";
 import { TransactionService } from "@/services/transactions_services";
+import usePeriodicStudentUpdate from "@/hooks/usePeriodicStudentUpdate";
 
 
 export const responsiveCarouselDashboard = {
@@ -49,7 +50,12 @@ export default function HomeLoggedIn() {
   });
 
   const student = useAppSelector(selectUser)
-  
+
+  // No componente onde você chama o hook
+  const adhesionNumber = student?.adhesionNumber ? String(student.adhesionNumber) : "";
+  usePeriodicStudentUpdate({ studentAdhesionNumber: adhesionNumber });
+
+
   const TransactsService = new TransactionService()
 
   // Função assíncrona para buscar as transações
