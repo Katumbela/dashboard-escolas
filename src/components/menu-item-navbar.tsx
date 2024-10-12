bdef6cc18dcc7310e5f26db015900707a4ae7541
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-12 19:48:17 +0100

    fix: src/components/menu-item-navbar.tsx. Criação da função MenuItem em src/components/menu-item-navbar.tsx; Modificação de código em src/components/menu-item-navbar.tsx; Alteração de texto em src/components/menu-item-navbar.tsx; Alteração em componente React em src/components/menu-item-navbar.tsx; Mudança de texto em um elemento HTML em src/components/menu-item-navbar.tsx; Alteração em funções de manipulação de eventos em src/components/menu-item-navbar.tsx; Alteração de conteúdo em array ou objeto em src/components/menu-item-navbar.tsx

--- src/components/menu-item-navbar.tsx
+++ src/components/menu-item-navbar.tsx
@@ -1,13 +1,13 @@
-import { NavLink } from "react-router-dom";
+import Link from "next/link";
 
 export function MenuItem({ icon: Icon, label, link, click }: { icon: React.ElementType, label: string, link?: string, click?: () => void }) {
   return (
     <div onClick={() => !link && click ? click() : null}>
-      <NavLink to={link ? link : "#"}
+      <Link href={link ? link : "#"}
         className="flex items-center px-4 py-3 space-x-3 transition-all rounded-md focus-within:bg-slate-200 bg-slate-100">
         <Icon className="text-alate-500" />
         <span>{label}</span>
-      </NavLink>
+      </Link>
     </div>
   );
 }
