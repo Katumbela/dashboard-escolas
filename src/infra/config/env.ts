9cb9381fbf6d2c8d7bac7900193230d1a452ac46
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-12 19:48:21 +0100

    fix: src/infra/config/env.ts. Modificação de código em src/infra/config/env.ts; Criação de uma constante em src/infra/config/env.ts

--- src/infra/config/env.ts
+++ src/infra/config/env.ts
@@ -1,5 +1,5 @@
 //const apiUrl = 'http://ec2-35-170-249-32.compute-1.amazonaws.com/api/v1'
-const apiUrl = 'https://multischooll-api.onrender.com'
+const apiUrl = 'https://multischooll-api-nest.vercel.app/'
 const localDb = 'http://localhost:3500'
 //const apiUrl = 'http://localhost:4000'
 
