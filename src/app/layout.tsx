659287eaf65d4c97935bfee4ca3f702256c56059
Joao Afonso Katombela <ja3328173@gmail.com>
2024-10-12 21:44:07 +0100

    fix: src/app/layout.tsx. Criação da função RootLayout em src/app/layout.tsx; Modificação de código em src/app/layout.tsx; Alteração de texto em src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import "./global.css";
-import { persistor, store } from '@/store';
+import { persistor, selectUser, store } from '@/store';
 import { Provider } from 'react-redux';
 import { ModalProvider } from '@/contexts';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from "react-toastify";
 import { Toaster } from "react-hot-toast";
+import { useAppSelector } from "@/hooks";
+import usePeriodicStudentUpdate from "@/hooks/usePeriodicStudentUpdate";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="pt">
       <body>
