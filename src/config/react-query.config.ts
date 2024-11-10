d1e1fe824a4dc232b4bf981578b5752acfdf0878
Manuela de Oliveira <manu.elad3oliveira@gmail.com>
2024-11-10 20:08:32 +0100

    Remove

--- src/config/react-query.config.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultOptions } from "@tanstack/react-query";
-
-const queryConfig: DefaultOptions = {
-  queries: {
-    refetchOnWindowFocus: false,
-  },
-};
-
-export default queryConfig;
--- src/lib/query-utils/get-query-client.tsx
+++
@@ -1,5 +0,0 @@
-import { useQueryClient } from "@tanstack/react-query";
-import { cache } from "react";
-
-const getQueryClient = cache(() => useQueryClient());
-export default getQueryClient;
